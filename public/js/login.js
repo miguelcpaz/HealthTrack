35cd23bd60c80cce3c16b200bdbe585afbbbdf6f
miguelcpaz <miguelc.schramm@gmail.com>
2025-10-15 23:11:35 -0300

    Update API endpoint to relative URL

--- public/js/login.js
+++ public/js/login.js
@@ -26,7 +26,7 @@ form.addEventListener("submit", async function (event) {
   const data = { email, senha };
 
   try {
-    const response = await fetch('http://localhost:3000/api/auth/login', {
+    const response = await fetch(/api/auth/login, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data)
