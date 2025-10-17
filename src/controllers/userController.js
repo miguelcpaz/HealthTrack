3d9b804cadc02060bf38e1b28ec45ab9a241c008
miguelcpaz <miguelc.schramm@gmail.com>
2025-10-16 22:08:19 -0300

    Update email transport secure setting to false
    Change email secure setting to false for transport.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -57,7 +57,7 @@ await prisma.solicitation.create({
     const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
-  secure: process.env.EMAIL_SECURE === "true", // false para 587
+  secure: process.env.EMAIL_SECURE === "false", // false para 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, // app password do Gmail
